8926c568451fd3816a2d9324e831095712740a63
Addison Wilberg <addisonwilberg@addisons-air-7.trinity.local>
2024-08-29 14:53:33 -0500

    uploading in-class exercise

---
+++ Exc.java
@@ -0,0 +1,53 @@
+import java.util.HashSet;
+
+public class Exc{
+    // First multiples of 5 and 7 \\
+    public int b = 5;
+        public int c = 7;
+        public int count;
+
+    public Exc(){
+        count = 0;
+    }
+    public int mult(int a){
+        
+        while(b < a || c < a){
+        if(b < a){
+            count += b;
+        }
+        if(c < a){
+            count += c;
+        }
+        b += 5;
+        c += 7;
+        }
+
+        return count;
+    }
+
+    // Repeated integers in arrays \\
+    public boolean check(int[] a){
+        HashSet<Integer> Set = new HashSet<>();
+        for(int i = 0; i < a.length; i++){
+            Set.add(a[i]);
+        }
+        if(Set.size() < a.length){
+            return true;
+        }
+        
+        return false;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Exc exc = new Exc();
+        int result = exc.mult(20);
+        System.out.println("Result: " + result);
+        int[] test = {1,2,3,4};
+        System.out.println(exc.check(test));
+    }
+
+ 
+}
